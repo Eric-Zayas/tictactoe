ba31e7e4d80856a24850acf498e2f26da7043653
Eric Zayas <ezayas16@icloud.com>
2017-06-26 20:09:15 -0400

    turn clarification

--- tictactoe.js
+++ tictactoe.js
@@ -4,31 +4,36 @@ let tictactoeMatrix = [
   [" ","|"," ","|"," "],
   [" ","|"," ","|"," "],
   [" ","|"," ","|"," "],
-  
 ];
 
 let renderMatrix =  "   1   2   3 \n" + "1  " + tictactoeMatrix[0].join(' ') + "\n" + "   __________  \n" + "2  " + tictactoeMatrix[1].join(' ') + " \n" + "   __________ \n" + "3  " + tictactoeMatrix[2].join(' ');
 console.log(renderMatrix);
 
+prompt.start();
+let count = 0; 
+count % 2 === 0 ?  console.log("Player X's turn ") : console.log("Player O's turn");
 
+prompt.get(['position'], function (err, result) {
+  if (err) { return onErr(err); }
+  console.log('Command-line input received:');
+  
+  placePosition(result.position);
+  count ++;
+  if(count < 9) prompt.start();
+});
 
-  prompt.start();
+function onErr(err) {
+  console.log(err);
+  return;
+}
 
-  prompt.get(['position'], function (err, result) {
-    if (err) { return onErr(err); }
-    console.log('Command-line input received:');
-    // console.log('  Position: ' + result.position);
-    placePosition(result.position);
-  });
+const playerTurn = function(){
+  
+}
 
-  function onErr(err) {
-    console.log(err);
-    return 1;
-  }
+const placePosition = function(position){
 
-var placePosition = function(positionArr){
-  // JSON.parse(positionArr);
-  console.log("pos arr", positionArr[1])
+  console.log("pos arr", positionArr[1]);
   tictactoeMatrix[positionArr[0]][positionArr[2]] = "X";
   renderMatrix =  "   1   2   3 \n" + "1  " + tictactoeMatrix[0].join(' ') + "\n" + "   __________  \n" + "2  " + tictactoeMatrix[1].join(' ') + " \n" + "   __________ \n" + "3  " + tictactoeMatrix[2].join(' ');
   console.log(renderMatrix);
