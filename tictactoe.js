e0235dcb214cf9cc9cd84446db5d8009372723de
Eric Zayas <ezayas16@icloud.com>
2017-06-26 23:10:34 -0400

    board fully functional

--- tictactoe.js
+++ tictactoe.js
@@ -24,10 +24,27 @@ const nextTurn = function(){
   prompt.get(['position'], function (err, result) {
   if (err) { return onErr(err); }
   placePosition(result.position);
-  });
+});
+count % 2 === 0 ?  console.log("Player X's turn ") : console.log("Player O's turn");
 }
 
-count % 2 === 0 ?  console.log("Player X's turn ") : console.log("Player O's turn");
+const correctedInput = function(position){
+  let yAxis = Number(position[0]);
+  let xAxis = Number(position[2]);
+  console.log("position",position);
+  xAxis -= 1; 
+  if(yAxis === 1){
+    yAxis = 0; 
+  } else if(yAxis === 2){
+    yAxis = 2;
+  } else if(yAxis === 3){
+    yAxis = 4;
+  }
+  console.log('x,y', xAxis, yAxis);
+ 
+  return xAxis + ',' + yAxis;
+}
+
 
 console.log("Example input: \n" + " 1,2");
 console.log("Example output: \n" + exampleMatrix);
@@ -36,15 +53,14 @@ console.log(renderMatrix);
 
 nextTurn();
 
-function onErr(err) {
-  console.log(err);
-  return;
-}
+function onErr(err) {console.log(err); return; }
+const placePosition = function(position){
+  if(position === "exit") return;
+  // console.log('correctedInput',correctedInput(position))
+  let correctIndex = correctedInput(position);
+  console.log("cI",correctIndex);
 
-const placePosition = function(positionArr){
-  if(positionArr === "exit") return;
-  
-  tictactoeMatrix[positionArr[0]][positionArr[2]] = playerToken;
+  tictactoeMatrix[correctIndex[0]][correctIndex[2]] = playerToken;
   playerToken === "X" ? playerToken = "O" : playerToken = "X";
   renderMatrix =  "   1   2   3 \n" + "1  " + tictactoeMatrix[0].join(' ') + "\n" + "   __________  \n" + "2  " + tictactoeMatrix[1].join(' ') + " \n" + "   __________ \n" + "3  " + tictactoeMatrix[2].join(' ');
   console.log(renderMatrix);
@@ -54,6 +70,5 @@ const placePosition = function(positionArr){
   } else {
     return "Game Over!";
   } 
-  
   
 }
